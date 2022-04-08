2ef01ee541060177f56ff956c46428c64f372f82
Teodora Sandu <86596578+teodorasandu291200@users.noreply.github.com>
2022-04-08 17:59:53 +0100

    Update BookLibrary.sol
    Updates BookLibrary with more solidity concept features

--- BookLibrary.sol
+++ BookLibrary.sol
@@ -7,115 +7,88 @@ import "./Owner.sol";
 contract BookLibrary is Owner {
 
     uint public bookCount = 0;
-    uint public numberOfOwners = 0;
-    uint private totalBookCount = 0;
+    uint public totalBookCount = 0;
+    uint randNonce = 0;
 
-    event NewBook(uint bookId, uint copies,string message);
-    event BorrowBook(uint bookId,  uint copies, string  message);
-    event ReturnBook(uint bookId, uint copies, string message);
+    event NewBook(string name, uint bookId, uint copies,string message);
+    event BorrowBook(string name,uint bookId,  uint copies, string  message);
+    event ReturnBook(string name,uint bookId, uint copies, string message);
 
 
     struct Book {
-        uint bookId;
+        string name;
         uint copies;
         bool isAvailable;
+        uint ownerCount;
+        mapping(uint256 => address) ownersHistory;
     }
 
-
-    struct History {
-        uint bookId;
-        address owner;
-        uint ownerBooks;
-    }
+    function randMod(string memory _name) internal returns(uint) {
+    randNonce++;
+    return uint(keccak256(abi.encodePacked(_name))) ;
+  }
 
     uint public bookId;
-    uint public historyId;
 
+    uint[] public listOfBookIds;
+    mapping(string => uint) public nameToId;
+    mapping(string => bool) private isPresent;
     mapping(uint => Book) public books;
-    mapping(uint => History) public bookHistory;
 
 
-    function _addBooks(uint _bookId, uint _copies) internal {
-        Book memory book =  Book(_bookId, _copies, true);
-        books[_bookId] = book;
+    function _addBooks(string memory _name, uint _copies) internal {
+        if(!isPresent[_name]) {
+            uint _bookId = randMod(_name);
+            listOfBookIds.push(_bookId);
+            nameToId[_name] = _bookId;
+        }
+        Book storage book = books[nameToId[_name]];
+        book.name = _name;
+        book.copies = _copies;
         bookCount = bookCount + _copies;
         totalBookCount = totalBookCount + _copies;
-        book.copies = book.copies + _copies;
         book.isAvailable = true;
+        isPresent[_name] = true;
 
-
-
-        emit NewBook(bookId++, _copies,"You have added books,owner");
+        emit NewBook(_name, nameToId[_name], _copies,"You have added books,owner");
 
     }
 
 
-    function _borrowBook(uint _bookId, uint _copies ) public  {
-        require(bookId >= 0  && _copies == 1, "You can borrow one copy at a time");
-        Book storage book = books[_bookId];
+    function _borrowBook(string memory _name,  uint _copies ) public {
+        require(_copies == 1, "You can borrow one copy at a time");
+        Book storage book = books[nameToId[_name]];
+        require(isPresent[_name] == true, "You are trying to borrow a book that doesn't exist");
         require(book.isAvailable == true, "The book is currently on loan");
         book.copies = book.copies - _copies;
         bookCount = bookCount - _copies;
-        bookHistory[_bookId].owner = msg.sender;
-        if(book.copies == 0) {
+        book.ownersHistory[nameToId[_name]] = msg.sender;
+        book.ownerCount = book.ownerCount + 1;
+        if(book.copies == 1) {
             book.isAvailable = false;
         }
-        _createHistory(_bookId, _copies);
 
-
-        emit BorrowBook(_bookId, historyId++,"You have borrowed books");
+        emit BorrowBook(_name,nameToId[_name], _copies,"You have borrowed books");
     }
 
 
-    function _returnBook(uint _bookId, uint _copies) public {
-        require(bookHistory[_bookId].owner == msg.sender, "Requester not the same as owner");
-        require(bookCount < totalBookCount ,"Trying to return too many books");
-        Book storage book = books[_bookId];
-        book.copies = book.copies + _copies;
-        History memory history = bookHistory[_bookId];
-        bookCount = bookCount + _copies;  
-        bookHistory[_bookId].ownerBooks =  _copies;
-        history.owner = address(0);
+    function _returnBook(string memory _name, uint _copies) public {
+        Book storage book = books[nameToId[_name]];
+        require(book.ownersHistory[nameToId[_name]] == msg.sender, "Requester not the same as owner");
+        require(bookCount + _copies <= totalBookCount , "Trying to add too many boooks");
 
+        bookCount = bookCount + _copies;
+        book.copies = book.copies + _copies;
         book.isAvailable = true;
 
-
-        emit ReturnBook(_bookId, _copies,"You have returned books");
-
-    }
-
-
-    function getAllAvailableBooks() public view returns (uint256[] memory) {
-        uint currentNumber = 0;
-        for (uint i = 1; i <= bookCount; i++) {
-            if (books[i].isAvailable == true) {
-                currentNumber++;
-            }
-        }
-        uint256[] memory result = new uint256[](currentNumber);
-        currentNumber = 0;
-        for (uint i = 1; i <= bookCount; i++) {
-            if (books[i].isAvailable == true) {
-                result[currentNumber] = i;
-                currentNumber++;
-            }
-        }
-        return result;
+        emit ReturnBook(_name,nameToId[_name], _copies,"You have returned books");
 
     }
 
-    function getOwnerHistoryOfBook(uint _bookId) public view returns (address[] memory) {
-        address[] memory result = new address[](bookCount);
-        for (uint i = 0; i  < numberOfOwners; i ++) {
-            result[i] = bookHistory[_bookId].owner;
-        }
-        return result;
-    }
+    function _returnBookArrayLength() public view returns (uint _bookArrayLength) {
 
-    function _createHistory(uint _bookId, uint _modifiedBookCount) internal { 
-        bookHistory[historyId] = History(_bookId,  msg.sender, _modifiedBookCount);
-        Book storage book = books[_bookId];
-        numberOfOwners++;
+        return listOfBookIds.length;
 
     }
+
 }
